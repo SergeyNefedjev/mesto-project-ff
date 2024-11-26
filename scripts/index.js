f9b207db6ae53205499f87fa3e165ada97220c7e
Sergey Nefedjev <sergeynefedjev@mail.ru>
2024-11-26 18:58:33 +0900

    второе ревью

--- scripts/index.js
+++ scripts/index.js
@@ -7,43 +7,27 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
-// const container = document.querySelector('.content');
 function renderCard() {
-  initialCards.forEach(({name,link}) => {
-    const card = addCard({name,link}, deleteCard);
+  const placesList = document.querySelector('.places__list');
+  initialCards.forEach(({name,link,alt}) => {
+    const card = addCard({name,link,alt});
+    placesList.append(card); 
   })
 }
 
-function addCard ({name, link}, deleteCard) {
-  const placesList = document.querySelector('.places__list');
+function addCard ({name, link, alt}) {
   const cardTemplate = document.querySelector('#card-template').content;
-  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true); 
   const deleteButton = cardElement.querySelector('.card__delete-button');
-
-  deleteButton.addEventListener('click', deleteCard);
-
   cardElement.querySelector('.card__image').src = link;
+  cardElement.querySelector('.card__image').alt = alt;
   cardElement.querySelector('.card__title').textContent = name;
-  
-  placesList.append(cardElement); 
+  deleteButton.addEventListener('click', () => deleteCard(cardElement));
+  return cardElement;
 }
 
-function deleteCard(){
-  const cardDelete = document.querySelector('.places__item');
-  cardDelete.remove();
+function deleteCard(cardElement) {
+  cardElement.remove();
 }
 
 renderCard();
-
-// const cardTemplate = document.querySelector('#card-template').content;
-// const cardsAdded = document.querySelector('.cards-added');
-
-// // клонируем содержимое тега template
-// const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-
-// // наполняем содержимым
-// cardElement.querySelector('.card__image').src = 'tinyurl.com/v4pfzwy';
-// cardElement.querySelector('.card__title').textContent = 'Дюк Корморант';
-
-// // отображаем на странице
-// cardsAdded.append(cardElement); 
